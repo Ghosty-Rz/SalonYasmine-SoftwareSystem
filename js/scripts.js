2f42ab15e86d3d74d68db6b265f081722421352a
storm2121 <stormcaller420@gmail.com>
2023-04-30 05:28:15 +0300

    working firebase version with edit/view buttons working

--- js/scripts.js
+++ js/scripts.js
@@ -152,47 +152,41 @@ function fetchServices() {
   database.ref('/services').on('value', (snapshot) => {
     servicesTableBody.innerHTML = ''; // Clear the existing table content
 
-    if (snapshot.exists()) {
-      snapshot.forEach((childSnapshot) => {
-        const service = childSnapshot.val();
-        const serviceKey = childSnapshot.key;
-        
-        // Create a new table row
-        const row = document.createElement('tr');
+    snapshot.forEach((childSnapshot) => {
+      const service = childSnapshot.val();
+      const serviceKey = childSnapshot.key;
+      
+      // Create a new table row
+      const row = document.createElement('tr');
 
-        // Add service name
-        const nameCell = document.createElement('td');
-        nameCell.textContent = service.serviceName;
-        row.appendChild(nameCell);
+      // Add service name
+      const nameCell = document.createElement('td');
+      nameCell.textContent = service.serviceName;
+      row.appendChild(nameCell);
 
-        // Add service price
-        const priceCell = document.createElement('td');
-        priceCell.textContent = service.servicePrice + ' Dh';
-        row.appendChild(priceCell);
+      // Add service price
+      const priceCell = document.createElement('td');
+      priceCell.textContent = service.servicePrice + ' Dh';
+      row.appendChild(priceCell);
 
-        // Add view and edit buttons
-        const viewCell = document.createElement('td');
-        const viewButton = document.createElement('button');
-        viewButton.textContent = 'View';
-        viewButton.addEventListener('click', () => viewService(serviceKey));
-        viewCell.appendChild(viewButton);
-        row.appendChild(viewCell);
+      // Add view and edit buttons
+      const viewCell = document.createElement('td');
+      const viewButton = document.createElement('button');
+      viewButton.textContent = 'View';
+      viewButton.addEventListener('click', () => viewService(serviceKey));
+      viewCell.appendChild(viewButton);
+      row.appendChild(viewCell);
 
-        const editCell = document.createElement('td');
-        const editButton = document.createElement('button');
-        editButton.textContent = 'Edit';
-        editButton.addEventListener('click', () => editService(serviceKey));
-        editCell.appendChild(editButton);
-        row.appendChild(editCell);
+      const editCell = document.createElement('td');
+      const editButton = document.createElement('button');
+      editButton.textContent = 'Edit';
+      editButton.addEventListener('click', () => editService(serviceKey));
+      editCell.appendChild(editButton);
+      row.appendChild(editCell);
 
-        // Add the row to the table
-        servicesTableBody.appendChild(row);
-      });
-    } else {
-      console.log('No data in the /services node.');
-    }
-  }, (error) => {
-    console.error('Error fetching data:', error);
+      // Add the row to the table
+      servicesTableBody.appendChild(row);
+    });
   });
 }
 
@@ -227,11 +221,102 @@ document.addEventListener('DOMContentLoaded', () => {
 // Function to handle View button click
 function viewService(serviceKey) {
   console.log('Viewing service with key:', serviceKey);
-  // Add your code to view the service details
+  database.ref(`/services/${serviceKey}`).once('value', (snapshot) => {
+    const service = snapshot.val();
+    viewServiceDetails(service);
+  });
 }
 
 // Function to handle Edit button click
 function editService(serviceKey) {
   console.log('Editing service with key:', serviceKey);
-  // Add your code to edit
+  database.ref(`/services/${serviceKey}`).once('value', (snapshot) => {
+    const service = snapshot.val();
+    editServiceDetails(serviceKey, service);
+  });
 }
+
+function viewServiceDetails(service) {
+  // Create a modal to display the service details
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  document.body.appendChild(modal);
+
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+  modal.appendChild(modalContent);
+
+  const closeBtn = document.createElement('span');
+  closeBtn.classList.add('close');
+  closeBtn.innerHTML = '&times;';
+  closeBtn.onclick = () => modal.remove();
+  modalContent.appendChild(closeBtn);
+
+  const serviceDetails = document.createElement('div');
+  serviceDetails.innerHTML = `
+    <h2>${service.serviceName}</h2>
+    <p>Description: ${service.serviceDescription}</p>
+    <p>Price: ${service.servicePrice} Dh</p>
+    <p>Category: ${service.serviceCategory}</p>
+    <p>Duration: ${service.serviceDuration} minutes</p>
+    <p>Employee's wage percentage: ${service.servicePercentage}%</p>
+  `;
+  modalContent.appendChild(serviceDetails);
+}
+
+function editServiceDetails(serviceKey, service) {
+  // Create a modal with a form to edit the service details
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  document.body.appendChild(modal);
+
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+  modal.appendChild(modalContent);
+
+  const closeBtn = document.createElement('span');
+  closeBtn.classList.add('close');
+  closeBtn.innerHTML = '&times;';
+  closeBtn.onclick = () => modal.remove();
+  modalContent.appendChild(closeBtn);
+
+  const editForm = document.createElement('form');
+  editForm.innerHTML = `
+    <label>Service Name</label>
+    <input type="text" name="serviceName" value="${service.serviceName}">
+    <label>Service Description</label>
+    <textarea>${service.serviceDescription}</textarea>
+    <label>Service Price</label>
+    <input type="number" name="servicePrice" value="${service.servicePrice}">
+    <label>Category</label>
+    <input type="text" name="serviceCategory" value="${service.serviceCategory}">
+    <label>Duration</label>
+    <input type="number" name="serviceDuration" value="${service.serviceDuration}">
+    <label>Employee's wage percentage</label>
+    <input type="number" name="servicePercentage" value="${service.servicePercentage}">
+    <input type="submit" value="Save Changes">
+  `;
+  modalContent.appendChild(editForm);
+
+  editForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(editForm);
+    const updatedService = {
+      serviceName: formData.get('serviceName'),
+      serviceDescription: formData.get('serviceDescription'),
+      servicePrice: formData.get('servicePrice'),
+      serviceCategory: formData.get('serviceCategory'),
+      serviceDuration: formData.get('serviceDuration'),
+      servicePercentage: formData.get('servicePercentage')
+    };
+
+    database.ref(`/services/${serviceKey}`).update(updatedService).then(() => {
+      console.log('Service updated successfully');
+      modal.remove();
+    }).catch((error) => {
+      console.error('Failed to update service:', error);
+    });
+  });
+}
+
